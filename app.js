a0a6ab83e4a8944be79763b9798b12c680eb0daa
AndreyShuma <0662389612a@gmail.com>
2023-06-09 09:56:48 +0300

    09-06-23

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const mongoose = require('mongoose');
 const UsrsModel = require('./models/user');
 
-mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+
 
 const data = {
-    name: 'Andrey',
-    surname: 'Shumeyko',
-    birthday: Date.now()
+    role: ['admin', 'manager', 'user'],
+    name: 'Тарас',
+    surname: '',
+    birthday: 1814,
+    today: Date.now()
 };
 
-UsrsModel.create(data);
+const testFunc = async () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/myapp');
+    await UsrsModel.create(data);
+   
+    await UsrsModel.updateOne({ name: 'Тарас' }, { name: 'Олег' });
+    await UsrsModel.deleteOne({birthday: 1814 });
+    const result = await UsrsModel.find({ surname: 'Шевченко' });
+    console.log(result);
+};
+
+testFunc();
+
--- models/user.js
+++ models/user.js
@@ -2,15 +2,18 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const schema = new Schema({
+    role: [{type: String}],
     name: { type: String},
     surname :{type: String},
-    birthday: { type: Date}
+    birthday :{type: Number},
+    today: { type: Date}
   });
   
   const model = mongoose.model('user', schema);
+  
   module.exports = model;
 
-const test = { a:10, b:20};
-const {a, b} = test;
-console.log('a>>>', a);
-console.log('b>>>', b);
+// const test = { a:10, b:20};
+// const {a, b} = test; 
+// console.log('a>>>', a);
+// console.log('b>>>', b);
